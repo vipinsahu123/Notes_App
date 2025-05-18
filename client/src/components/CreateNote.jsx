17dc91b8f765d74501b72d60aac5676d98403b9d
Vipin Sahu <vipinsahuji123@gmail.com>
2025-05-18 18:21:55 +0530

    replace localhost url

--- client/src/components/CreateNote.jsx
+++ client/src/components/CreateNote.jsx
@@ -11,7 +11,7 @@ const CreateNote = () => {
     e.preventDefault();
 
     // Send data to backend
-    axios.post("http://localhost:3001/notes", { title, content })
+    axios.post("https://notes-app-server-ag2f.onrender.com/notes", { title, content })
       .then(() => {
         setTitle("");
         setContent("");
